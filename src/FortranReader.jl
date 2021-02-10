3bb7dc8506a0aef8c7a77253aafe86f8e941dcf4
Andy Nowacki <a.nowacki@leeds.ac.uk>
2021-02-10 12:26:39 +0000

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.13

---
+++ src/FortranReader.jl
@@ -0,0 +1,5 @@
+module FortranReader
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using FortranReader
+using Test
+
+@testset "FortranReader.jl" begin
+    # Write your tests here.
+end
